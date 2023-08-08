ec0e365a8056270dce3f56edf1646c9e0ea07cc8
christianinacio <christianginacio@gmail.com>
2023-08-07 23:10:43 -0300

    requirements

--- __init__.py
+++ __init__.py
@@ -2,14 +2,15 @@ from models.Operation import Operation
 from Interface import Interface
 import os
 
-option = Interface.options()
-os.system('clear')
-while(option != "4"):
-    if option == '1':
-        Interface.insertOperation()
-    elif option == '2':
-        Interface.StatisticMenu()
-    os.system('clear')
+if __name__ == "__main__":
     option = Interface.options()
     os.system('clear')
+    while(option != "4"):
+        if option == '1':
+            Interface.insertOperation()
+        elif option == '2':
+            Interface.StatisticMenu()
+        os.system('clear')
+        option = Interface.options()
+        os.system('clear')
 
